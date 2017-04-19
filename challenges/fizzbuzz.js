1d81fe8195b5ba48e1147a58943d73ccb5b287e3
eva luna <evaluna@evas-MacBook-Air-2.local>
2017-04-19 09:55:08 -0700

    finished challenge

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -15,8 +15,27 @@
 //                     14,
 //                     'fizzbuzz',
 //                     16 ]
-
+//success = retuns array with nums 1-n, with all numbers divisible by  3 (n%3===0) with "fizz" and numbers divisible by 5 (n%5===0) with buzz.
+// iterate thru array and for each number check if divisible by 3, add fizz to output string if true and then check if divisible by 5 and add string "buzz" to output fi true.
+//if none of the conditions where met then just push the number to the array, else push output atring.
 function fizzbuzz(num) {  
+	const result = [];
+	for(let i = 1 ; i< num; i++) {
+		let string = '';
+		if (i%3 === 0) {
+			string += 'fizz';
+		}
+		if (i%5 === 0) {
+			string += 'buzz';
+		}
+		if (!string) {
+			result.push(n);
+		}
+		else {
+			result.push(string);
+		}
+	}
+	return result;
 
 }
 
