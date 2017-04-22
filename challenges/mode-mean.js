2c5c9cdc5563b854364560c67c50518dfcd353f0
Joshua Wolters <joshuawolters@Joshuas-MacBook-Pro.local>
2017-04-22 09:56:53 -0700

    completed mode-mean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,26 @@
 
 
 function modemean(array) {
+// determine mean
+sum = array.reduce((acc,curr)=>acc+curr);
+mean = Math.floor(sum/array.length);
+
+// return mean
+meanObj = array.reduce((acc,curr)=>{
+    acc[curr] = acc[curr] + 1 || 1
+    return acc
+},{})
+
+numbers = Object.keys(meanObj);
+values = Object.values(meanObj);
+maxValue = values.reduce((max,curr)=>Math.max(max,curr));
+mode = Math.floor(numbers[values.indexOf(maxValue)])
+
+//determine if they're equal
+return (mean === mode)
 
 }
+
+// console.log(modemean([1,2,3,4,5,3,3]))
 
 module.exports = modemean;
