054e57de7eccacd22d9fadecef98a17f9ad58e10
Aaron Goldsmith <aarongoldsmith@msn.com>
2017-04-22 09:41:39 -0700

    done

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,26 @@
 
 
 function modemean(array) {
-
+  let freq = [];
+  //calculate average rounding down
+  let mean =  Math.floor(array.reduce((acc, next) => acc + next) / array.length);
+  //find frequency of each number
+  let countedNums = array.reduce((allNums, num) => {
+    if (num in allNums) {
+      allNums[num]++;
+    } else {
+      allNums[num] = 1;
+    }
+    return allNums;
+    }, {});
+  //store all frequencies
+  for (let key in countedNums){
+    freq.push(countedNums[key])
+    }
+  //sort modes to find largest
+  let mode = freq.sort((a, b) => b-a)[0]
+  //return if mean and mode are equal
+  return mean === mode
 }
 
 module.exports = modemean;
