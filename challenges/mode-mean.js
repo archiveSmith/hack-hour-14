132a01fc5030579f808bf45da0587afbf2f10ef6
DebraD <debdo@rocketmail.com>
2017-04-22 09:42:27 -0700

    mode-mean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -10,8 +10,30 @@
  */
 
 
-function modemean(array) {
+ function modemean(array) {
+   let sum = 0;
+   let modeList = {};
+   let mode = 0;
+   let highest = 0;
 
-}
+   sortedArray = array.sort();
+   for (let i = 0; i < sortedArray.length; i++) {
+     let value = sortedArray[i];
+     sum += value;
+     if (modeList[value] === undefined) {
+       modeList[value] = 1;
+     }
+     else {
+       modeList[value]++;
+     }
+     if (modeList[value] >= highest) {
+       mode = value;
+     }
+   }
+
+   let mean = Math.floor(sum/sortedArray.length);
+
+   return mode === mean;
+ }
 
 module.exports = modemean;
