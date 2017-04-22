70ceb24f9014cf3cddb51beaee74a903f25de44d
Poyu Chen <p820316@gmail.com>
2017-04-22 10:03:04 -0700

    Finished modemean challenge

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -10,8 +10,36 @@
  */
 
 
-function modemean(array) {
+ function modemean(array) {
+ 	let mean = Math.floor(array.reduce((acc, cur) => {
+ 		return acc + cur;
+ 	}, 0) / array.length);
 
-}
+ 	let modeObj = array.reduce((acc, cur) => {
+ 		if (acc[cur] === undefined) {
+ 			acc[cur] = 1;
+ 		}else if (acc.hasOwnProperty(cur)) {
+ 			acc[cur] += 1;
+ 		}
+ 		return acc;
+ 	}, {});
 
-module.exports = modemean;
+ 	let mode = array.sort((a, b) => {
+ 		return modeObj[a] - modeObj[b];
+ 	}).reverse();
+
+ 	let max;
+ 	mode.forEach((cur) => {
+ 		if (max === undefined) {
+ 			max = cur;
+ 		} else if (max !== cur) {
+ 			if (modeObj[max] === modeObj[cur]) {
+ 				max = max > cur ? max : cur;
+ 			}
+ 		}
+ 	});
+
+ 	return max == mean;
+ }
+
+ module.exports = modemean;
