8295b180da66962ceb4129c4edc6c295d57c8178
John Ivan <johnbauzon@gmail.com>
2017-04-22 10:10:11 -0700

    modeMeanv1

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,31 @@
 
 
 function modemean(array) {
-
+  array = array.sort();
+  let sum = 0;
+  let modeMap = {};
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i]
+    if (modeMap[array[i]]) {
+      modeMap[array[i]] = modeMap[array[i]] + 1
+    } else {
+      modeMap[array[i]] = 1;
+    }
+  }
+  let mode = 0
+  for (prop in modeMap) {
+    if (prop > mode)
+      mode = prop;
+  }
+  let mean = Math.floor(sum / array.length)
+  // console.log(modeMap);
+  // console.log(mode);
+  // console.log(mean);
+  if (mean == mode) {
+    return true
+  } else {
+    return false
+  }
 }
 
 module.exports = modemean;
