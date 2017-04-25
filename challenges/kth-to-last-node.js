d24361db7f89e72adf1839867c44d34d7a0b817e
Joshua Wolters <joshuawolters@Joshuas-MacBook-Pro.local>
2017-04-25 09:50:54 -0700

    completed kthtolastnode

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -22,7 +22,32 @@ function Node(val) {
 }
 
 function kthToLastNode(k, head) {
+  if(k<=0){return undefined}
+  let arrayValues = [];
+  let counter = 0;
 
+  while(head){
+    arrayValues.push(head.value)
+    head = head.next;
+  }
+
+  if(k > 0 && k <= arrayValues.length){
+    return arrayValues[arrayValues.length - k]
+  } else { return -1}
 }
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
+
+
+// const a = new Node('A');
+// const b = new Node('B');
+// const c = new Node('C');
+// const d = new Node('D');
+// const e = new Node('E');
+
+// a.next = b;
+// b.next = c;
+// c.next = d;
+// d.next = e;
+
+// console.log(kthToLastNode(2, a))
