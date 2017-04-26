56ba0d85126f31c41f80c152ca78367a7d133686
Brian Feldman <brianfeldman@Caitlins-MacBook-Pro.local>
2017-04-26 09:42:31 -0700

    Hack Hour 042617 Complete

---
+++ challenges/pow.js
@@ -0,0 +1,21 @@
+/* Write a function that calculates x^y, where x is given as the base and y is given as the power.
+ * Use recursion!
+ */
+
+// function powNoRe(base, power) {
+// let value=base
+// for (let i=power-1;i>0;i--){
+//     value = value*base
+// }
+// return value;
+// }
+
+function pow(base,power){
+    if(power === 0){return 1};
+    if(power ===1){return base};
+    let value;
+    value = base * powRe(base, power-1)
+    return value;
+}
+
+module.exports = pow;
