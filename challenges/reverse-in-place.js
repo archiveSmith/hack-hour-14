4179ca9726395fd329ee3cf0b89f49b6eb0b81c5
jacobworrel <jacobworrel@gmail.com>
2017-04-28 09:45:48 -0700

    reverseInPlace first solution

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -14,7 +14,15 @@
  */
 
 function reverseInPlace(array) {
-
+  let mid = Math.floor((array.length)/2);
+  for (let i = 0; i < mid; i += 1) {
+    let prev = array[i];
+    array[i] = array[array.length - 1  - i];
+    array[array.length - 1 - i] = prev;
+  }
+  return array;
 }
+
+console.log(reverseInPlace(['a','b','c','d', 'e']));
 
 module.exports = reverseInPlace;
