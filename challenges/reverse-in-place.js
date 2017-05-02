40e570532d6ddb4c4bac52ae8fcf579c2d04c9a7
Developing Dev <developingdev@users.noreply.github.com>
2017-05-02 09:18:52 -0700

    conflict fixed

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -15,9 +15,9 @@
 
 function reverseInPlace(array) {
     for (let i = 0; i < array.length / 2; i++) {
-        let temp = array[i];
-        array[i] = array[array.length - i - 1];
-        array[array.length - i - 1] = temp;
+        // let temp = array[i];
+        [array[i], array[array.length - i - 1]] = [array[array.length - i - 1], array[i]];
+        // array[array.length - i - 1] = temp;
     }
 
     return array;
