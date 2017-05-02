137d47c6712bc97dcaa05638e60e2a8836781709
John Ivan <johnbauzon@gmail.com>
2017-05-02 10:03:24 -0700

    balanced parens v1

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -14,7 +14,7 @@
  * Example:
  *  balancedParens('[](){}'); // true
  *  balancedParens('[({})]');   // true
- *  balancedParens('[(]{)}'); // false
+ *  balancedParens('[(]{)}'); // false - won't pass this
  *
  * Step 3:
  * ignore non-bracket characters
@@ -24,8 +24,31 @@
  *
  */
 
-function balancedParens(input){
-
+function balancedParens(input) {
+  const arr = input.split('');
+  // console.log(arr);
+  let leftBracket = 0;
+  let leftParens = 0;
+  let leftCurly = 0;
+  let rightBracket = 0;
+  let rightParens = 0;
+  let rightCurly = 0;
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] === '[' && arr.indexOf(']') > i &&
+    arr.indexOf('(') > arr.indexOf(']') &&
+    arr.indexOf('{') > arr.indexOf(']')) { leftBracket += 1; }
+    else if (arr[i] === ']') { rightBracket += 1; }
+    else if (arr[i] === '(' && arr.indexOf(')') > i) { leftParens += 1; }
+    else if (arr[i] === ')') { rightParens += 1; }
+    else if (arr[i] === '{' && arr.indexOf('}') > i ) { leftCurly += 1; }
+    else if (arr[i] === '}') { rightCurly += 1; }
+  }
+  if (leftBracket === rightBracket && leftParens === rightParens && leftCurly === rightCurly) {
+    return true;
+  }
+  return false;
 }
+
+console.log(balancedParens('[(]{)}'));
 
 module.exports = balancedParens;
