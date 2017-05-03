c78ae6263ab03daac7bbc71859b2130591994bef
Luke <luke.michals@gmail.com>
2017-05-03 08:56:36 -0700

    upload 5/3

---
+++ challenges/balanced-parens.js
@@ -0,0 +1,37 @@
+/*
+ * write a function that takes a string of text and returns true if
+ * the parentheses are balanced and false otherwise.
+ *
+ * Example:
+ *   balancedParens('(');  // false
+ *   balancedParens('()'); // true
+ *   balancedParens(')(');  // false
+ *   balancedParens('(())');  // true
+ *
+ * Step 2:
+ *   make your solution work for all types of brackets
+ *
+ * Example:
+ *  balancedParens('[](){}'); // true
+ *  balancedParens('[({})]');   // true
+ *  balancedParens('[(]{)}'); // false
+ *
+ * Step 3:
+ * ignore non-bracket characters
+ * balancedParens(' var wow  = { yo: thisIsAwesome() }'); // true
+ * balancedParens(' var hubble = function() { telescopes.awesome();'); // false
+ *
+ *
+ */
+
+function balancedParens(input){
+    // base case
+    if (input.length===0){return true}
+
+    if (input[0]===input[input.length-1]){
+        
+    }
+
+}
+
+module.exports = balancedParens;
