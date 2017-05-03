a55e69850516049c11b190a87526e2a2c84e80d4
Joshua Wolters <joshuawolters@Joshuas-MacBook-Pro.local>
2017-05-03 09:52:03 -0700

    completed roman numeral

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -17,8 +17,27 @@
  * 
  */
 
-function romanNumeral(n) {
+//basecase = k
 
+function romanNumeral(n,string = '') {
+    let numerals = {1: "I",4:"IV",5:"V",9:"IX",10:"X",40:"XL",50:"L",90:"XC",100:"C",400:"CD",500:"D",900:"CM",1000:"M"}
+    let numbers = Object.keys(numerals).reverse()
+
+    if(n === 1) {
+        string += "I"
+        return string;
+    }
+
+    for(let num in numbers){
+        if(n - numbers[num]>0){
+            string += numerals[numbers[num]];
+            string = romanNumeral(n-numbers[num],string);
+            break;
+        }
+    }
+
+    return string;
 }
+
 
 module.exports = romanNumeral;
