3bc7ba45e73c4a5677b935d6148d6f51b79f58f4
jacobworrel <jacobworrel@gmail.com>
2017-05-03 22:50:56 -0700

    roman num done

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -14,11 +14,36 @@
  *      500   ->    D
  *      900   ->    CM
  *      1000  ->    M
- * 
+ *
  */
 
-function romanNumeral(n) {
-
+const map = {
+  'M': 1000,
+  'CM': 900,
+  'D': 500,
+  'CD': 400,
+  'C': 100,
+  'XC': 90,
+  'L': 50,
+  'XL': 40,
+  'X': 10,
+  'IX': 9,
+  'V': 5,
+  'IV': 4,
+  'I': 1
 }
+
+function romanNumeral(n) {
+  let result = '';
+  for (let key in map) {
+    while (n >= map[key]) {
+      result += key;
+      n -= map[key];
+    }
+  }
+  return result;
+}
+
+console.log(romanNumeral(1001))
 
 module.exports = romanNumeral;
