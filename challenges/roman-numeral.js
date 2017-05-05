f536cc41afec24e11b8421e400f71b147d74e26e
eva luna <evaluna@evas-MacBook-Air-2.local>
2017-05-05 09:41:19 -0700

    fixed roman numerals

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -16,36 +16,23 @@
  *      1000  ->    M
  * 
  */
-
 function romanNumeral(n) {
-    if(n<0) return -1;
 
     let dictionary = {I :1, IV : 4,  V :5 , IX: 9,  X : 10,  XL : 40, L : 50, XC: 90, C : 100, CD: 400, D: 500,  CM: 900, M : 1000}  
-    
-    return findBiggestChunk(n)
 
-}
-
-function findBiggestChunk(n, dictionary) {
-    //get biggest composition
-    let maxNumber  = infinity;
+    let maxNumber= 0;
     let maxNumeral = ""
 
-    //loop thru dictionary
     for(d in dictionary) {
         //base case
         if(n-dictionary[d] === 0) return d;     
         
-        //whichever value takes the input closest to 0 will be returned as the biggest chunk
-        if(n-dictionary[d] > 0 && n-dictonary[d] < maxNumber) {
+        if(n-dictionary[d] >= 0 && dictionary[d] > maxNumber) {
              maxNumber = dictionary[d];
              maxNumeral = d;  
         } 
-                
     }
-    //current biggest numeral then add the next biggest numeral
-    return maxNumeral + findBiggestChunk(n-maxNumber);
+    return maxNumeral += romanNumeral(n-maxNumber);
 }
-       
 
 module.exports = romanNumeral;
