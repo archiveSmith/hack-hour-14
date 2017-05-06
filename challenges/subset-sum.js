ba7aa0814c1c2c97f5ef667a061fcac9309dfd41
eva luna <evaluna@evas-MacBook-Air-2.local>
2017-05-06 09:39:06 -0700

    subset

--- challenges/subset-sum.js
+++ challenges/subset-sum.js
@@ -8,8 +8,40 @@
  * subsetSum([8, -2, 1, -3], 6) -> true, 8 + 1 + (-3) = 6
  */
 
-function subsetSum(array, target) {
+function subsetSum(array, target, selectorSize) {
+    //get all possible permutations of adding the sub set sums together
+    const uniqueNumbers = [];
+   // let selectorSize = 0;
 
+    //if selector Size is 0 then return number 
+    //if(selector Size === 0 ) {
+        //return number
+    //}
+
+    //for selector size from 0 to array length, with 0 just being the element itself
+    //for each digit
+    for(let d = 0; d < array.length; d++) {
+        //current digit
+        let digit = array[d];
+
+        //get array without element
+        let rest = array.splice(d,1);
+
+        //add digit with each selectorSize chunk of the rest array 
+        //get selectorSize by breaking array into sub array
+
+
+        //and push to unique numnbers if unique
+
+
+
+
+    }
+
+    //check if target matches any numbers in the unique array
+    // if(uniqueArray.indexOf(target) > 0) return true;
+    // return false;
+    
 }
 
 module.exports = subsetSum;
