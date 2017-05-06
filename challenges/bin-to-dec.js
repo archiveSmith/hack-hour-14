c99c778b9aed92aec3199d6c9d0b697ce562a343
Developing Dev <developingdev@users.noreply.github.com>
2017-05-06 09:41:59 -0700

    bin to dec v1

---
+++ challenges/bin-to-dec.js
@@ -0,0 +1,41 @@
+/* You are given a string that represents a binary number
+ *
+ * Write a function that converts the binary string to a decimal number
+ *
+ * Example:
+* 	binToDec('0')   -> 0
+ * 	binToDec('11')  -> 3
+ * 	binToDec('100') -> 4
+ * 	binToDec('101') -> 5
+ *  binToDec('0101') -> 5
+ *
+ * Extension:
+ * Write a function that converts a decimal number to binary (then maybe hexadecimal)
+ */
+
+function binToDec(binary) {
+    // reverse string
+    // iterate each char
+    // double multiplier
+    let total = 0;
+    let value = 1;
+    for (let i = 0; i < binary.length; i++) {
+
+        if (binary.charAt(binary.length - 1 - i) === '1') {
+            total += value;
+        };
+
+        // console.log(value);
+        value *= 2;
+    }
+
+    return total;
+}
+
+console.log(binToDec('0')) //   -> 0
+console.log(binToDec('11')) //  -> 3
+console.log(binToDec('100')) // -> 4
+console.log(binToDec('101')) // -> 5
+console.log(binToDec('0101')) // // -> 5
+
+module.exports = binToDec;
