b06018588657c5fbb67d1f754505ffd19d7610e6
Brian Feldman <brianfeldman@Caitlins-MacBook-Pro.local>
2017-05-10 11:35:32 -0700

    HackHour 051017 v3

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,13 +3,26 @@
  * Given an array of integers, find the highest product you can get from three of the integers.
  */
 function highestProduct(array) {
-    function sortNumber(a,b) {
-    return a - b;
-}
-    let returnArr = array.sort(sortNumber);
     let value = 1;
-    for(let i =returnArr.length-1; i>=0 && i>=returnArr.length-3; i--){
-    value = value*returnArr[i];
+    let negativeCount = 0;
+    let topThree = [];
+    //populate the top 3 largest absolute values
+    let returnArr = array.sort((a, b) => { return Math.abs(b) - Math.abs(a) });
+    for (let i = 0; i < returnArr.length && i < 3; i++) {
+        topThree.push(returnArr[i])
+        if (returnArr[i] < 0) { negativeCount++ }
+    }
+    //if 0 or 2 of the top 3 absolute values are negative, we can use the current top 3 (as populated)
+    if (negativeCount === 0 || negativeCount === 2) {
+      console.log('in neg')
+        topThree.forEach(function (ele) { value *= ele });
+    }
+    //otherwise we re-sort without absolute values and use the new top 3
+    else {
+        returnArr = array.sort((a,b)=> {return b-a});
+        for (let i = 0; i < returnArr.length && i < 3; i++) {
+          value *= returnArr[i];
+        }
     }
     return value;
 }
