f52e11e2cd86fe8c754b083f06b5e7641f676be8
Alyssa Gaudioso <alyssa.gaudioso@gmail.com>
2017-05-10 11:24:31 -0700

    highprod with neg numbers

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -5,7 +5,12 @@
 function highestProduct(array) {
     if(array.length < 3) return 'array must be at least three';
     array.sort((a,b)=>(b-a));
+    console.log(array);
     if(isNaN(array[0]) || isNaN(array[1]) || isNaN(array[2]) ) return 'array must be of numbers';
-    else return array[0]*array[1]*array[2];
+    if(array.length > 5 && !isNaN(array[array.length-2]) && !isNaN(array[array.length-1])){
+        return array[0]*array[array.length-2]*array[array.length-1];
+    }
+    return array[0]*array[1]*array[2];
 }
-module.exports = highestProduct;
+// module.exports = highestProduct;
+console.log(highestProduct([-98,-100,324,123,231,1,1,4,]));
