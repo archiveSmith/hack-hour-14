533c4209470e66264abc78c2f3bccbdd1fb0dbf2
Alyssa Gaudioso <alyssa.gaudioso@gmail.com>
2017-05-11 09:56:20 -0700

    zip-linked-list

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,14 +3,16 @@
  * Given an array of integers, find the highest product you can get from three of the integers.
  */
 function highestProduct(array) {
-    if(array.length < 3) return 'array must be at least three';
+    if(array.length < 3){
+        return 'array must be at least three';
+    }
     array.sort((a,b)=>(b-a));
-    console.log(array);
-    if(isNaN(array[0]) || isNaN(array[1]) || isNaN(array[2]) ) return 'array must be of numbers';
+    if(isNaN(array[0]) || isNaN(array[1]) || isNaN(array[2]) ){
+        return 'array must be of numbers';
+    }
     if(array.length > 5 && !isNaN(array[array.length-2]) && !isNaN(array[array.length-1])){
         return array[0]*array[array.length-2]*array[array.length-1];
     }
     return array[0]*array[1]*array[2];
 }
-// module.exports = highestProduct;
-console.log(highestProduct([-98,-100,324,123,231,1,1,4,]));
+module.exports = highestProduct;
