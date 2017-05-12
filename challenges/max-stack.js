29bc360ce4a4b680806d4dff69860e607058f234
Poyu Chen <p820316@gmail.com>
2017-05-12 09:55:27 -0700

    ES6 all the things!

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -6,36 +6,37 @@
  * BONUS: The getMax method should retrieve the maximum value from the stack in O(1) time.
  */
 
-function Stack() {
-  this.store = {};
-  this.index = 0;
-  this.max;
-}
+class Stack {
+  constructor() {
+    this.store = {}
+    this.index = 0
+    this.max
+  }
 
-Stack.prototype.push = function(item) {
-  this.store[this.index++] = item;
-  if (!this.max) this.max = item;
-  else this.max = (item > this.max) ? item : this.max;
-  return this.index;
-}
+  push(item) {
+    this.store[this.index++] = item
+    if (!this.max) this.max = item
+    else this.max = (item > this.max) ? item : this.max
+    return this.index
+  }
 
-Stack.prototype.pop = function() {
-  let result = this.store[--this.index];
-  if (result !== this.max) return result;
-  else {
-    let curMax;
-    for (let i = 0; i < this.index; i++) {
-      if (i === 0) curMax = this.store[i];
-      if (this.store[i] > curMax) curMax = this.store[i];
+  pop() {
+    let result = this.store[--this.index]
+    if (result !== this.max) return result
+    else {
+      let curMax
+      for (let i = 0; i < this.index; i++) {
+        if (i === 0) curMax = this.store[i]
+        if (this.store[i] > curMax) curMax = this.store[i]
+      }
+      this.max = curMax
+      return result
     }
-    this.max = curMax;
-    return result;
+  }
+
+  getMax() {
+    return this.max
   }
 }
 
-
-Stack.prototype.getMax = function() {
-  return this.max;
-}
-
-module.exports = Stack;
+module.exports = Stack
