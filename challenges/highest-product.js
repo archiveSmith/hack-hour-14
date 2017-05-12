0187e30a5c1358899d97b282c1819cb5e7fd5605
John Ivan <johnbauzon@gmail.com>
2017-05-12 09:23:40 -0700

    hack hr progress

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,18 +3,23 @@
  */
 
 function highestProduct(array) {
-  array.sort((a, b) => {
-    return a - b;
-  });
-  const bigThree = array.slice(array.length - 3);
-  console.log(bigThree);
-  return bigThree.reduce((acc, val) => {
-    return acc * val;
-  });
+  if (array.length < 3) { return 0; }
+  array.sort((a, b) => { return a - b; });
+  if (array[array.length - 1] < 0) {
+    const bigThree = array.slice(array.length - 3);
+    // console.log(bigThree);
+    return bigThree.reduce((acc, val) => {
+      return acc * val;
+    });
+  }
+  const highestNumber = array[array.length - 1];
+  const lowProduct = array[0] * array[1];
+  const highProduct = array[array.length - 2] * array[array.length - 3];
+  return (lowProduct > highProduct) ? lowProduct * highestNumber : highProduct * highestNumber;
 }
 
 // fix to take into account negative numbers
 
-console.log(highestProduct([-1, -2, 1, -3]));
+// console.log(highestProduct([-1, -5, 1, 2]));
 
 module.exports = highestProduct;
