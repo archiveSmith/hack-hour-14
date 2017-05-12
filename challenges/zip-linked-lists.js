5d885ccf99bfce77576be9b1d6098d95576c3b86
DebraD <debdo@rocketmail.com>
2017-05-12 09:48:20 -0700

    rename zip-linked-list.js to zip-linked-lists.js

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,48 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
-};
+  if (!l1 && !l2) {
+    return null;
+  }
+
+  if (!l1) {
+    return l2;
+  }
+
+  if (!l2) {
+    return l1;
+  }
+
+  let head = l1;
+  let tail = l1;
+  
+  let l1Head = l1.next;
+  let l2Head = l2;
+  let first = false;
+  
+  while (l1Head && l2Head) {
+    if (first) {
+      tail.next = l1Head;
+      tail = tail.next;
+      l1Head = l1Head.next;
+      first = false;
+    }
+    else {
+      tail.next = l2Head;
+      tail = tail.next;
+      l2Head = l2Head.next;
+      first = true;
+    }
+  }
+  
+  if (l1Head) {
+    tail.next = l1Head;
+  }
+  else if (l2Head) {
+    tail.next = l2Head;
+  }
+  
+  return head;
+}
 
 module.exports = {Node: Node, zip: zip};
