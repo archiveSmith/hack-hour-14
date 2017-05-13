da4f3b317717cbeb163776d2eec687063f272aef
Joshua Wolters <joshuawolters@Joshuas-MacBook-Pro.local>
2017-05-13 09:44:15 -0700

    completed reverse linked list

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -14,7 +14,30 @@ function Node(value) {
 }
 
 function reverseLinkedList(head) {
-
+    //create new linkedlist
+    let linkNew;
+    //create a tracking node value
+    let prevNode = null;
+    //while the current node value is not null
+    while(head){
+        linkNew = new Node(head.value)
+        linkNew.next = prevNode;
+        prevNode = linkNew;
+        head = head.next;
+    }
+    return linkNew
 }
+
+// let testHead = new Node(0)
+// let test1= new Node(1)
+// let test2= new Node(2)
+// let test3= new Node(3)
+
+// testHead.next = test1;
+// test1.next = test2;
+// test2.next = test3;
+
+// // console.log(testHead)
+// reverseLinkedList(testHead)
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
