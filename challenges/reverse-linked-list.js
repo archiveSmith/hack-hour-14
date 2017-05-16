982ea6b34be0668abaef0edccb51cc4d15452ad3
jordanpina <jordan.t.pina@gmail.com>
2017-05-16 09:19:29 -0700

    commit

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -14,7 +14,25 @@ function Node(value) {
 }
 
 function reverseLinkedList(head) {
+  let curr = head;
+  let nextNode = head.next;
+  let prevNode;
 
+
+  while(curr){
+
+  }
+
+  return curr;
 }
+
+let node1 = new Node(1);
+let node2 = new Node(2);
+let node3 = new Node(3);
+node1.next = node2;
+node2.next = node3;
+console.log(reverseLinkedList(node1));
+
+
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
