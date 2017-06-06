e89cc571cb3b942b39f795071438103d7884e843
Batul Kanawati <Batul Kanawati>
2017-06-06 09:49:33 -0700

    done

--- challenges/count-twos.js
+++ challenges/count-twos.js
@@ -8,7 +8,17 @@
 
 
 function countTwos(num) {
-
+  let count = 0;
+  for (let i = 0; i <= num; i++) {
+    if (i.toString().indexOf('2') > -1) {
+      for (let j = 0; j < i.toString().length; j++) {
+        if (i.toString()[j] === '2'){
+          count++;
+        }
+      }
+    }
+  }
+  return count;
 }
 
 module.exports = countTwos;
