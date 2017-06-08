81202881dffa0cd8e79231bc157b0e154eeb73e9
Poyu Chen <p820316@gmail.com>
2017-06-08 10:10:41 -0700

    Finished superbalaned

--- challenges/superbalanced.js
+++ challenges/superbalanced.js
@@ -4,17 +4,28 @@
 1) Left subtree of T is balanced
 2) Right subtree of T is balanced
 3) The difference between heights of left subtree and right subtree is not more than 1.
-example: http://www.geeksforgeeks.org/wp-content/uploads/balanced_tree.GIF
+example:  
  */
 
-function BinaryTree(value) {
-  this.value = value;
-  this.left = null;
-  this.right = null;
+class BinaryTree {
+  constructor (value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
 }
 
-function superbalanced(tree) {
+const superbalanced = (tree) => {
+  if (tree.left === null && tree.right === null) return true
+  const diff = Math.abs(findHeight(tree.left) - findHeight(tree.right))
+  return diff <= 1
+}
 
+const findHeight = (tree) => {
+    if (tree === null) return 0
+    let leftH = findHeight(tree.left)
+    let rightH = findHeight(tree.right)
+    return (leftH > rightH) ? leftH + 1 : rightH + 1
 }
 
 module.exports = {BinaryTree: BinaryTree, superbalanced: superbalanced};
