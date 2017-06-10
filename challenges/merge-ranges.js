590f23e0c7dc5eeb44d40431314caad609073112
Brian Feldman <brianfeldman@Caitlins-MacBook-Pro.local>
2017-06-10 10:07:06 -0700

    hack hour 061017 complete

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -9,9 +9,23 @@
  * Do not assume the ranges are in order
  */
 
-
 function mergeRanges(array) {
-
+    let sorted = array.sort((a,b)=> a[0]-b[0]);
+    let output= [sorted[0]];
+    sorted.forEach((ele) =>{
+        if(ele[0]<output[output.length-1][1]){
+            if(ele[1]>output[output.length-1][1]){
+                output[output.length-1][1]=ele[1];
+            }
+        }
+        else if(ele[0]>output[output.length-1][1]){
+            output.push(ele);
+        }
+        else if(ele[1]>output[output.length-1][1]){
+            output[output.length-1][1]=ele[1];
+        }
+    })
+    return output;
 }
 
 module.exports = mergeRanges;
