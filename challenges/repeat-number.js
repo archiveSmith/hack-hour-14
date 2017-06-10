a314ed2047b193d69b6814bddec784afdc33af76
Luke <luke.michals@gmail.com>
2017-06-10 09:12:03 -0700

    repeat-number submission 1

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -10,8 +10,32 @@
  *
  */
 
-function repeatNumbers(array) {
 
+// Solution 1
+    // O(n) time
+    // O(n) space
+// function repeatNumbers(array) {
+//     const storage = {};
+//     for (let i=0 ; i<array.length ; i++){
+//         if (!storage[array[i]]) storage[array[i]] = 1;
+//         else return array[i];
+//     }
+// }
+
+// Solution 2
+    // O(n) time
+    // O(n) space
+function repeatNumbers(array) {
+    let dup;
+    array.sort((a,b)=>{
+        console.log('a -->', a)
+        console.log('b -->', b)
+        return a-b;
+    });
+    return array;
 }
+
+const array = [1,2,3,4,2]
+console.log(repeatNumbers(array));
 
 module.exports = repeatNumbers;
