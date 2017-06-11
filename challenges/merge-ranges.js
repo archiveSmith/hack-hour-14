a1c49ea6a1e0692102f554c3eb92c759ac25107b
jacobworrel <jacobworrel@gmail.com>
2017-06-11 13:39:52 -0700

    merge ranges check for encapsulated times

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -17,29 +17,26 @@ function mergeRanges(array) {
   //if first item in array[i] < last item in array[i - 1], merge those two arrays
   //else keep looping
 
-  const sorted = array.sort((a,b) => {
-    return a[0] - b[0];
-  });
+  const sorted = array.sort((a,b) => a[0] - b[0]);
 
   for (let i = 1; i < sorted.length; i += 1) {
 
     const curr = sorted[i];
     const prev = sorted[i - 1];
-    // console.log('curr', curr);
-    // console.log('prev', prev);
+    const next = sorted[i + 1];
+
+    if (next && next[0] >= prev[0] && next[next.length - 1] <= curr[curr.length - 1]) sorted.splice(i + 1, 1);
 
     if (curr[0] <= prev[prev.length - 1]) {
       const merged = prev.concat(curr);
       merged.splice(1, merged.length - 2);
-      // console.log('merged', merged);
       sorted.splice(i - 1, 2, merged);
-      // console.log('sorted', sorted);
     }
   }
   return sorted;
 }
 
-var times = [[9, 10], [3, 5], [4, 8], [0, 1], [10, 12]]
+var times = [[9, 10], [3, 5], [6, 7], [4, 8], [0, 1], [10, 12]]
 
 console.log(mergeRanges(times)); //-> [[0, 1], [3, 8], [9, 12]]
 
