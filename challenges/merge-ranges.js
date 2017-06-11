b417465baa0a1a6d2171e96589f8a4f596f13f38
jacobworrel <jacobworrel@gmail.com>
2017-06-11 15:06:30 -0700

    merge ranges commented and cleaned up

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,42 +11,34 @@
 
 
 function mergeRanges(array) {
-  //first item in array must be bigger than last item in prev array
-  //sort nested arrays
-  //loop through array
-  //if first item in array[i] < last item in array[i - 1], merge those two arrays
-  //else keep looping
 
-  const sorted = array.sort((a,b) => {
+  array.sort((a,b) => {
+    //if first item in both arrays is the same, sort them according to last item
     if (a[0] === b[0]) return a[1] - b[1];
     return a[0] - b[0]
   });
 
-  console.log(sorted);
-  for (let i = 1; i < sorted.length; i += 1) {
-    // console.log(i)
+  for (let i = 1; i < array.length; i += 1) {
 
-    const curr = sorted[i];
-    const prev = sorted[i - 1];
-    const next = sorted[i + 1];
+    const curr = array[i];
+    const prev = array[i - 1];
+    const next = array[i + 1];
 
-    console.log('curr', curr);
-    console.log('prev', prev)
-    console.log('next', next)
+    //check for encapsulated times
+    if (next && next[0] >= prev[0] && next[next.length - 1] <= curr[curr.length - 1]) array.splice(i + 1, 1);
 
-    if (next && next[0] >= prev[0] && next[next.length - 1] <= curr[curr.length - 1]) sorted.splice(i + 1, 1);
-
-    if (curr && curr[0] <= prev[prev.length - 1]) {
+    //if first item in curr <= last item in prev, merge them
+    if (curr[0] <= prev[prev.length - 1]) {
       const merged = prev.concat(curr);
-      merged.sort((a,b) => a - b);
-      merged.splice(1, merged.length - 2);
-      console.log('merge', merged);
-      sorted.splice(i - 1, 2, merged);
-      console.log('sorted',sorted);
+      // sort merged and splice out all values except for first and last
+      merged.sort((a,b) => a - b).splice(1, merged.length - 2);
+      //splice out two overlapping arrays and insert merged array
+      array.splice(i - 1, 2, merged);
+      //decrement i (since we replaced two overlapping arrays with one merged array)
       i -= 1;
     }
   }
-  return sorted;
+  return array;
 }
 
 var times = [[9, 10], [3, 5], [6, 7], [4, 8], [0, 2], [0,1], [10,11], [10, 12], [0,12]];
