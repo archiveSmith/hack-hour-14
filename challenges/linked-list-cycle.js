553e40454867385e052b0a9f375824650e35bb40
Joshua Wolters <joshuawolters@Joshuas-MacBook-Pro.local>
2017-06-12 09:32:47 -0700

    completed linked-list-cycle

--- challenges/linked-list-cycle.js
+++ challenges/linked-list-cycle.js
@@ -33,7 +33,25 @@ var Node = function(value) {
 }
 
 function hasCycle(head) {
-
+  if(!head) return false
+  //story key-value pairs as you iterate through
+  let store = {};
+  //loop through the nodes
+  while(head.next){
+    if(store[head.next.value]) return true;
+    store[head.value] = head.next.value;
+    head = head.next
+  }
+  return false
 }
+
+// var node1 = new Node('1');
+// var node2 = node1.next = new Node('2');
+// var node3 = node2.next = new Node('3');
+// var node4 = node3.next = new Node('4');
+// var node5 = node4.next = new Node('5');
+// console.log(hasCycle(node1)); // => false
+// node5.next = node2;
+// console.log(hasCycle(node1)); // => true
 
 module.exports = {Node: Node, hasCycle: hasCycle}
