35084aa5aad49996170ccaff4095de6c9599f67c
John Ivan <johnbauzon@gmail.com>
2017-06-12 09:33:51 -0700

    ll-cycle v1

--- challenges/linked-list-cycle.js
+++ challenges/linked-list-cycle.js
@@ -30,10 +30,28 @@
 var Node = function(value) {
   this.value = value;
   this.next = null;
-}
+  // added this.passed
+  this.passed = false;
+};
 
 function hasCycle(head) {
-
+  // let currNode = head;
+  // loop through ll, if it has an end return false, else return true
+  while (head) {
+    if (head.next === null) return false;
+    head.passed = true;
+    head = head.next;
+    if (head.passed === true) return true;
+  }
 }
+
+// var node1 = new Node('1');
+// var node2 = node1.next = new Node('2');
+// var node3 = node2.next = new Node('3');
+// var node4 = node3.next = new Node('4');
+// var node5 = node4.next = new Node('5');
+// console.log(hasCycle(node1)); // => false
+// node5.next = node2;
+// console.log(hasCycle(node1)); // => true
 
 module.exports = {Node: Node, hasCycle: hasCycle}
