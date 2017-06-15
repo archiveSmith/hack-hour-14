1e44d2f920eeabf6702cf9326bf504519b7b598f
Jordan Pina <jordan@Jordans-MacBook-Air.local>
2017-06-15 09:51:14 -0700

    anagrams

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -12,8 +12,48 @@
   * console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-function anagrams(string) {
+// function anagrams(string) {
+//   let arr = [];
 
+//   for(let i = 0; i < string.length; i++){
+    
+//   }
+  
+
+// }
+console.log(getAnagrams('abc'));
+// module.exports = anagrams;
+
+function swap(chars, i, j) {
+    var tmp = chars[i];
+    chars[i] = chars[j];
+    chars[j] = tmp;
 }
 
-module.exports = anagrams;
+function getAnagrams(input) {
+    var counter = [],
+        anagrams = [],
+        chars = input.split(''),
+        length = chars.length,
+        i;
+
+    for (i = 0; i < length; i++) {
+        counter[i] = 0;
+    }
+
+    anagrams.push(input);
+    i = 0;
+    while (i < length) {
+        if (counter[i] < i) {
+            swap(chars, i % 2 === 1 ? counter[i] : 0, i);
+            counter[i]++;
+            i = 0;
+            anagrams.push(chars.join(''));
+        } else {
+            counter[i] = 0;
+            i++;
+        }
+    }
+
+    return anagrams;
+}
