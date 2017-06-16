e05c8ed02bca4bcb47aca6d1818d17a1105f8172
Luke <luke.michals@gmail.com>
2017-06-16 09:27:56 -0700

    code structured

--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -10,10 +10,38 @@
  * How would you solve this problem if a temporary buffer is not allowed?
  */
 
-
+var Node = function (value) {
+    this.value = value;
+    this.next = null;
+}
 
 function deleteDups(head) {
+    // set up variables
+    let currNode = head;
+    let compNodeTail = head;
+    let compNode = head.next;
 
+    // loop over linked list
+    while (currNode) {
+        // for each node, loop over remaning of linked list
+        while (compNode) {
+            // check if there's a duplicate (value of currNode === value of comparisonNode)
+            if (currNode.value === compNode.value) {
+                compNodeTail.next = compNode.next;
+            }
+            compNodeTail = compNode;
+            compNode = compNode.next;
+        }
+        currNode = currNode.next;
+    }
 }
+
+let ll = new Node(1);
+ll.next = new Node(2);
+ll.next.next = new Node(3);
+ll.next.next.next = new Node(3);
+// console.log('ll --> ', ll);         // --> 1->2->3->3
+
+console.log('deleteDups(ll) --> ', deleteDups(ll));        // --> 1->2->3
 
 module.exports = deleteDups;
