12cbb8553cf28da3e38d2f92ba239faa2097ebed
jacobworrel <jacobworrel@gmail.com>
2017-06-16 09:38:02 -0700

    delete dups first try

--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -14,6 +14,40 @@
 
 function deleteDups(head) {
 
+  // const arr = [];
+  // let current = head;
+  // while (current) {
+  //   arr.push(current.value);
+  //   current = current.next;
+  // }
+  // const filtered = arr.filter((item, index, array) => {
+  //   return array.indexOf(item) === index;
+  // })
+  // let newList = null;
+  // for (let i = filtered.length - 1; i >= 0; i -= 1) {
+  //   newList = {
+  //     value: filtered[i],
+  //     next: newList
+  //   }
+  // }
+  // return newList;
+
+  const arr = [];
+  let current = head;
+  while (current) {
+    arr.push(current.value);
+    if (current.next && arr.includes(current.next.value)) {
+      let temp = current.next.next;
+      current.next = temp;
+    }
+    current = current.next;
+  }
+
+  return JSON.stringify(head);
 }
+
+const ll = { value: 1, next: { value: 1, next: { value: 3, next: { value: 3, next: null}}}};
+
+console.log(deleteDups(ll))
 
 module.exports = deleteDups;
