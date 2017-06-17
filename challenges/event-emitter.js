6ca7b9b339d7095f53984646e84ea550dc8ca718
John Ivan <johnbauzon@gmail.com>
2017-06-17 09:32:30 -0700

    event-emitter-v1

--- challenges/event-emitter.js
+++ challenges/event-emitter.js
@@ -22,15 +22,26 @@
  */
 
 function EventEmitter() {
-
+  this.eventStore = {};
 }
 
 EventEmitter.prototype.on = function(funcName, func) {
-
+  if (this.eventStore.funcName) return func
+  this.eventStore[funcName] = func;
+  return func;
 };
 
 EventEmitter.prototype.trigger = function(funcName, ...args) {
-
+  this.eventStore[funcName](...args);
 };
+
+// var instance = new EventEmitter();
+// var counter = 0;
+// instance.on('increment', function() {
+//   counter++;
+//  }); // counter should be 0
+//  // console.log(instance);
+//  instance.trigger('increment'); // counter should be 1
+ // instance.trigger('increment'); // counter should be 2
 
 module.exports = EventEmitter;
