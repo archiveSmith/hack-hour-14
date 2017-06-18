c76546b23feb594fa0e58e00380e1e4cc7e6eb68
Jordan Pina <jordan@Jordans-MacBook-Air.local>
2017-06-18 10:26:52 -0700

    bin to dec refactor

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -3,17 +3,29 @@
  * Write a function that converts the binary string to a decimal number
  *
  * Example:
-* 	binToDec('0')   -> 0
- * 	binToDec('11')  -> 3
- * 	binToDec('100') -> 4
- * 	binToDec('101') -> 5
- *  binToDec('0101') -> 5
+ 	binToDec('0')  // -> 0
+  	binToDec('11') // -> 3
+  	binToDec('100')// -> 4
+  	binToDec('101')// -> 5
+   binToDec('0101'//) -> 5
  *
  * Extension:
  * Write a function that converts a decimal number to binary (then maybe hexadecimal)
  */
 
 function binToDec(binary) {
+  if(!/^[01]+$/.test(binary)) return  'Please pass binary string';
+  //split binary into an array
+  //reverse the array
+  //loop over the array
+  return binary.split('').reverse().reduce((all, item, index)=>{
+  //if there is a 0 do not find decimal equivalent
+  //if there is a 1 find decimal equivalent
+    if(item === '1'){
+      all += Math.pow(2,index); 
+    }
+    return all
+  },0); 
 
     //    let binaryArr = binary.split('').reverse().map(function(item){
     //      return Number(item);
@@ -31,30 +43,30 @@ function binToDec(binary) {
     //     })
     //     console.log('output: '+output);
 
-    return binary.split('')
-                 .reverse()
-                 .reduce(function(all, item, index) {
-                   let num = Number(item) * Math.pow(2, index);
-                   all.push(num);
-                   return all;
-                 }, [])
-                 .reduce(function(a, b) {
-                   return a + b;
-                 });
+    // return binary.split('')
+    //              .reverse()
+    //              .reduce(function(all, item, index) {
+    //                let num = Number(item) * Math.pow(2, index);
+    //                all.push(num);
+    //                return all;
+    //              }, [])
+    //              .reduce(function(a, b) {
+    //                return a + b;
+    //              });
 
 
   //solution:
 
-  if(!/^[01]+$/.test(binary)) return  'Please pass binary string';
+//   if(!/^[01]+$/.test(binary)) return  'Please pass binary string';
 
-  const binArray = binary.split('').reverse();
-  let answer = 0;
-  for(let i = 0; i < binArray.length; i++){
-    if(binArray[i]==='1'){
-      answer += Math.pow(2,i);
-    }
-  }
-return answer;
+//   const binArray = binary.split('').reverse();
+//   let answer = 0;
+//   for(let i = 0; i < binArray.length; i++){
+//     if(binArray[i]==='1'){
+//       answer += Math.pow(2,i);
+//     }
+//   }
+// return answer;
 }
 
 module.exports = binToDec;
