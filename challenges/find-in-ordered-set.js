4e3aa5b763cbe32571c6cbaf2b8736e8cc2abffc
Jordan Pina <jordan@Jordans-MacBook-Air.local>
2017-06-19 09:50:31 -0700

    find in ordered set

---
+++ challenges/find-in-ordered-set.js
@@ -0,0 +1,42 @@
+/*
+How quickly could we check if a given integer is in the array?
+Assume the array is sorted.
+You can do better than O(n) time
+
+var nums = [1, 4, 6, 7, 9, 17, 45]
+findInOrderedSet(nums, 4);  -> true
+findInOrderedSet(nums, 2);  -> false
+
+ */
+
+//comeback to this one
+function findInOrderedSet(arr, target) {
+    let middleValue;
+    let middleIndex
+    let curr;
+  
+    
+    
+    middleIndex = Math.floor((arr.length / 2))
+    middleValue = arr[middleIndex];  
+    
+
+    if(target === middleValue){
+        return true; 
+    }
+    else if( target < middleValue){
+        curr = arr.slice(0,middleIndex) 
+       return findInOrderedSet(curr, target);
+    }else if(target > middleValue){
+        curr = arr.slice(middleIndex,arr.length-1) 
+        return findInOrderedSet(curr, target);
+    }
+    return false; 
+}
+
+var nums = [1, 4, 6, 7, 9, 17, 45]
+console.log(findInOrderedSet(nums, 4));  //-> true
+console.log(findInOrderedSet(nums, 2));  //-> false
+
+
+module.exports = findInOrderedSet;
