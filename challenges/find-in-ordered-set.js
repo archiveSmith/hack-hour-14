b04051a658fc0f09979a1d9cfb635234f75840ff
John Ivan <johnbauzon@gmail.com>
2017-06-19 09:35:53 -0700

    binary-search v1

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -9,10 +9,30 @@ findInOrderedSet(nums, 2);  -> false
 
  */
 
-
 function findInOrderedSet(arr, target) {
+  // binary search
+  let minIndex = 0;
+  let maxIndex = arr.length - 1;
+  let currentIndex;
+  let currentElement;
 
+  while (minIndex <= maxIndex) {
+    currentIndex = Math.floor((minIndex + maxIndex) / 2);
+    currentElement = arr[currentIndex];
+
+    if (currentElement < target) {
+      minIndex = currentIndex + 1;
+    } else if (currentElement > target) {
+      maxIndex = currentIndex - 1;
+    } else {
+      return true;
+    }
+  }
+  return false;
 }
 
+// var nums = [1, 4, 6, 7, 9, 17, 45]
+// console.log(findInOrderedSet(nums, 4)); // -> true
+// console.log(findInOrderedSet(nums, 2)); // -> false
 
 module.exports = findInOrderedSet;
