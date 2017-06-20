1c08b45b7be76b65299f6ebca10613cf75a40a06
John Ivan <johnbauzon@gmail.com>
2017-06-20 09:48:40 -0700

    insertion-sort v1

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -2,10 +2,27 @@
 
 // https://en.wikipedia.org/wiki/Insertion_sort
 
-
-
 function insertionSort(array) {
-
+  // loop through arr and store values using temp variable
+  for (let i = 1; i < array.length; i += 1) {
+    let temp = array[i];
+    // store the index of the previous value
+    let j = i - 1;
+    // while the index of the previous value is >= 0 && the previous value is
+    // greater than the current temp value
+    while ( j >= 0 && array[j] > temp) {
+      // insert the previous value in the current index
+      array[j+1] = array[j];
+      // decrement to move back one index
+      j -= 1;
+    }
+    // set the value of the prevIdx = the saved temp Value
+    array[j+1] = temp;
+  }
 }
 
 module.exports = insertionSort;
+
+// let arr = [3, 5, 3, 6]
+// insertionSort(arr);
+// console.log(arr);
