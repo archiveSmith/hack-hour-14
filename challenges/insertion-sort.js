5e5471de7adde954a6c3af17c40f04b77d4bf16a
DebraD <debdo@rocketmail.com>
2017-06-20 09:31:57 -0700

    insertion-sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,24 @@
 
 
 function insertionSort(array) {
-
+  for (let i = 1; i < array.length; i++) {
+    let j = i;
+    do {
+      if (array[j-1] > array[j]) {
+        let temp = array[j];
+        array[j] = array[j-1];
+        array[j-1] = temp;
+        swap = true;
+        j--;
+      }
+      else {
+        swap = false;
+      }
+    }
+    while (swap && j >= 0);
+  }
+  
+  return array;
 }
 
 module.exports = insertionSort;
