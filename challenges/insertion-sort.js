b9b8935990d51c916824bb9a7fda10012232cc08
Poyu Chen <p820316@gmail.com>
2017-06-20 09:30:36 -0700

    Finished insertion-sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,19 @@
 
 
 function insertionSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i + 1] < array[i]) {
+      let move = array.splice(i + 1, 1)[0];
+      for (let j = 0; j < array.length; j++) {
+        if (move < array[j]) {
+          array.splice(j, 0, move);
+          break;
+        }
+      }
+    }
+  }
 
-}
+  return array;
+ }
 
 module.exports = insertionSort;
