0e3d1309ffe3c95c5a2479616ff36f0283332c68
John Ivan <johnbauzon@gmail.com>
2017-06-21 09:59:35 -0700

    insertion sortv2

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -9,30 +9,42 @@ findInOrderedSet(nums, 2);  -> false
 
  */
 
-function findInOrderedSet(arr, target) {
+function findInOrderedSet(array, number) {
   // binary search
-  let minIndex = 0;
-  let maxIndex = arr.length - 1;
-  let currentIndex;
-  let currentElement;
+  // let minIndex = 0;
+  // let maxIndex = arr.length - 1;
+  // let currentIndex;
+  // let currentElement;
+  //
+  // while (minIndex <= maxIndex) {
+  //   currentIndex = Math.floor((minIndex + maxIndex) / 2);
+  //   currentElement = arr[currentIndex];
+  //
+  //   if (currentElement < target) {
+  //     minIndex = currentIndex + 1;
+  //   } else if (currentElement > target) {
+  //     maxIndex = currentIndex - 1;
+  //   } else {
+  //     return true;
+  //   }
+  // }
+  // return false;
 
-  while (minIndex <= maxIndex) {
-    currentIndex = Math.floor((minIndex + maxIndex) / 2);
-    currentElement = arr[currentIndex];
-
-    if (currentElement < target) {
-      minIndex = currentIndex + 1;
-    } else if (currentElement > target) {
-      maxIndex = currentIndex - 1;
-    } else {
-      return true;
-    }
+  if(!array.length) return false;
+  let middle = Math.floor(array.length/2);
+  if (array[middle] === number) {
+    return true;
   }
-  return false;
+  if (array[middle] > number) {
+    return findInOrderedSet(array.slice(0, middle - 1), number)
+  }
+  if (array[middle] < number) {
+    return findInOrderedSet(array.slice(middle+1, array.length -1))
+  }
 }
 
-// var nums = [1, 4, 6, 7, 9, 17, 45]
-// console.log(findInOrderedSet(nums, 4)); // -> true
-// console.log(findInOrderedSet(nums, 2)); // -> false
+var nums = [1, 4, 6, 7, 9, 17, 45]
+console.log(findInOrderedSet(nums, 4)); // -> true
+console.log(findInOrderedSet(nums, 2)); // -> false
 
 module.exports = findInOrderedSet;
