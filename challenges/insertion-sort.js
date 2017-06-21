d9a0d9db9c82ab6ef02938447b54115bc7b1788f
johnnunns <johndnunns@gmail.com>
2017-06-21 10:00:24 -0700

    insert sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,16 @@
 
 
 function insertionSort(array) {
-
+for(let i =1;i < array.length; i++){
+    let currVal = array[i]
+    let j;
+        for(let j = i-1; j>=0 && array[j] > currVal; j--){
+            array[j+1] = array[j];
+            array[j]= currVal;
+        }
+}
+return array
 }
 
+console.log(insertionSort([1,4,3,7,6,8]))
 module.exports = insertionSort;
