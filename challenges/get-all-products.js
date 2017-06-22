a69dde19dbbf5114962ab283e2bf5a886090d03f
Luke <luke.michals@gmail.com>
2017-06-22 09:37:08 -0700

    submission 1

--- challenges/get-all-products.js
+++ challenges/get-all-products.js
@@ -9,8 +9,20 @@
  * do not use division, becuase zero might be in the array and you cannot divide by zero
  */
 
+const log = console.log
+
 function getAllProducts(array) {
 
+    const output = []
+    let remaining;
+    for (let i=0 ; i<array.length ; i++){
+        remaining = array.slice(0,i).concat(array.slice(i+1))
+        output.push(remaining.reduce((a,v)=> a*v , 1))
+    }
+    return output
 }
+
+arr = [1, 7, 3, 4]
+log(getAllProducts(arr))    //  -->  [84, 12, 28, 21]
 
 module.exports = getAllProducts;
