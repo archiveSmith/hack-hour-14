8ff4e2f6dc0a5534970857c6d654e8dd34ca3656
John Ivan <johnbauzon@gmail.com>
2017-06-22 10:19:10 -0700

    productsv1

--- challenges/get-all-products.js
+++ challenges/get-all-products.js
@@ -10,7 +10,45 @@
  */
 
 function getAllProducts(array) {
+  if (array.length === 0) return [0];
+  const products = [];
+  // generate all unique combinations of an array
+  let combinations = combine(array, array.length - 1);
+  combinations = combinations.filter((el) => {
+    return el.length === array.length - 1 })
+  // console.log(combinations);
 
+  // get the product of each and return that array
+  combinations.forEach((el, i) => {
+    el = el.reduce((acc, val) => {return acc * val})
+    products.push(el);
+  });
+  return products
 }
+
+// helper from slack
+function combine(arr, min) {
+    var combis = [];
+    for (let i = min; i < arr.length; i += 1) {
+        fn(i, arr, [], combis);
+    }
+    combis.push(arr);
+    return combis;
+}
+
+function fn(n, src, got, all) {
+    if (n === 0) {
+        if (got.length > 0) {
+            all[all.length] = got;
+        }
+        return;
+    }
+    for (var j = 0; j < src.length; j++) {
+        fn(n - 1, src.slice(j + 1), got.concat([src[j]]), all);
+    }
+    return;
+}
+
+// console.log(getAllProducts([1, 7, 3, 4]));
 
 module.exports = getAllProducts;
