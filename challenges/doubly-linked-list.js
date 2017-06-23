da296b1b1ddfa0ebd098ecb9586829da566cf6e9
jacobworrel <jacobworrel@gmail.com>
2017-06-23 11:08:29 -0700

    doubly linked list first solution

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -17,14 +17,51 @@ function Node(val) {
 Adds a node to the end of the list
  */
 LinkedList.prototype.add = function(val) {
-  
+  let node = new Node(val);
+  if (!this.head) {
+    this.head = node;
+    this.tail = node;
+  }
+  else {
+    this.tail.next = node;
+    let temp = this.tail;
+    node.prev = temp;
+    this.tail = node;
+  }
 };
 
 /*
 Removes the first node with the inputted value
  */
 LinkedList.prototype.remove = function(val) {
-  
+  if (!this.head) return undefined;
+  else if (this.head.val === val) {
+    console.log('removed val in head')
+    this.head = this.head.next;
+    this.head.prev = null;
+  }
+  else {
+    let current = this.head.next;
+    while (current) {
+      if (current.val === val) {
+        console.log('removed from middle')
+        current.prev.next = current.next;
+      }
+      current = current.next;
+    }
+  }
+  return 'end of the list';
 };
+
+const list = new LinkedList();
+list.add(1);
+list.add(2);
+list.add(3);
+list.add(4);
+list.remove(4);
+console.log(list.head.val)
+console.log(list.head.next.val)
+console.log(list.head.next.next.val)
+console.log(list.head.next.next.next)
 
 module.exports = LinkedList;
