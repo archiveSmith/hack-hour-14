782aa9b559af2572c49c9e8d9d4c11dfb3ded17b
Poyu Chen <p820316@gmail.com>
2017-06-23 10:05:20 -0700

    Finished doubly-linked-list

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -2,29 +2,62 @@
 Create a doubly linked list with an add and remove method
  */
 
-function LinkedList() {
-  this.head = null;
-  this.tail = null;
+class LinkedList {
+  constructor () {
+    this.head = null
+    this.tail = null
+  }
+  
+  add (val) {
+    let newNode = new Node(val)
+    if (this.head === null) {
+      this.head = newNode
+      this.tail = newNode
+    } else {
+      newNode.prev = this.tail
+      this.tail.next = newNode
+      this.tail = newNode
+    }
+  }
+  
+  remove (val) {
+    if (this.head !== null) {
+      if (this.head.val === val) {
+        this.head = this.head.next
+        this.head.prev = null
+        return
+      }
+      for (let i = this.head; i !== null; i = i.next) {
+        if (i.next.val === val) {
+          if (i.next.next === null) {
+            i.next = null
+            this.tail = i
+          } else {
+            i.next = i.next.next
+            i.next.prev = i
+          }
+          break
+        }
+      }
+      return
+    }
+  }
+  
+  log () {
+    if (this.head !== null) {
+      for (let i = this.head; i !== null; i = i.next) {
+        console.log(i.val)
+      }
+    }
+  }
 }
 
-function Node(val) {
-  this.val = val;
-  this.next = null;
-  this.prev = null;
+class Node {
+  constructor (val) {
+    this.val = val
+    this.next = null
+    this.prev = null
+  }
 }
-
-/*
-Adds a node to the end of the list
- */
-LinkedList.prototype.add = function(val) {
-  
-};
-
-/*
-Removes the first node with the inputted value
- */
-LinkedList.prototype.remove = function(val) {
-  
-};
 
 module.exports = LinkedList;
