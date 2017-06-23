7eb8656e751bc2a437f7a735eafc67b8cbaeceb2
DebraD <debdo@rocketmail.com>
2017-06-23 09:33:21 -0700

    doubly-linked-list

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -13,18 +13,52 @@ function Node(val) {
   this.prev = null;
 }
 
-/*
-Adds a node to the end of the list
- */
 LinkedList.prototype.add = function(val) {
-  
+  if (!this.head) {
+    this.head = this.tail = new Node(val);
+  }
+  else {
+    let node = new Node(val);
+    node.prev = this.tail;
+    this.tail.next = node;
+    this.tail = node;
+  }
 };
 
 /*
 Removes the first node with the inputted value
  */
 LinkedList.prototype.remove = function(val) {
+  let currNode = this.head;
   
+  while (currNode) {
+    if (currNode.val === val) {
+      // currNode is head and tail
+      if (!currNode.prev && !currNode.next) {
+        this.head =  this.tail = null;
+        return;
+      }
+      // currNode is head
+      if (!currNode.prev) {
+        this.head = currNode.next;
+        currNode.next.prev = null;
+        return;
+      }
+      // currNode is tail
+      if (!currNode.next) {
+        currNode.prev.next = null;
+        this.tail = currNode.prev;
+        return;
+      }
+      // currNode is in middle
+      {
+        currNode.prev.next = currNode.next;
+        currNode.next.prev = currNode.prev;
+        return;
+      }
+    }
+    currNode = currNode.next;
+  }
 };
 
 module.exports = LinkedList;
