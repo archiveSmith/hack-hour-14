416362d8c6c7f0653c82363237257cac4f6c6900
jacobworrel <jacobworrel@gmail.com>
2017-06-24 09:51:05 -0700

    fixed tail

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -36,24 +36,23 @@ Removes the first node with the inputted value
 LinkedList.prototype.remove = function(val) {
   if (!this.head) return undefined;
   else if (this.head.val === val) {
-    console.log('off with the head!')
     this.head = this.head.next;
     this.head.prev = null;
+    return 'removed';
   }
   else {
     let current = this.head.next;
     while (current) {
       if (current.val === val) {
         current.prev.next = current.next;
-        if (current.next) {
-          console.log('removed from middle')
-          current.next.prev = current.prev;
-        }
+        if (current.next) current.next.prev = current.prev;
+        if (!current.next) this.tail = current.prev;
+        return 'removed';
       }
       current = current.next;
     }
   }
-  return 'end of the list';
+  return undefined;
 };
 
 const list = new LinkedList();
@@ -61,10 +60,12 @@ list.add(1);
 list.add(2);
 list.add(3);
 list.add(4);
-list.remove(4);
-console.log(list.head.val)
-console.log(list.head.next.val)
-console.log(list.head.next.next.val)
+// console.log(list.tail);
+console.log(list.remove(4));
+console.log(list.tail);
+// console.log(list.head.val)
+// console.log(list.head.next.val)
+// console.log(list.head.next.next.val)
 // console.log(list.head.next.next.next.val)
 
 module.exports = LinkedList;
