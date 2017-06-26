246a996afad7f0d7c64cbe14e18fcde9fb96e4e3
Luke <luke.michals@gmail.com>
2017-06-26 09:26:54 -0700

    submission 1

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -3,9 +3,33 @@
 // https://en.wikipedia.org/wiki/Bubble_sort
 
 
+const l = console.log
 
 function bubbleSort(array) {
 
+    let stopPoint = array.length;
+
+    // while stopPoint !== 0
+    while (stopPoint) {
+        // for loop through the array
+        for (let i = 0; i < stopPoint; i++) {
+            // compare each value to the next value
+            if (array[i] > array[i + 1]) {
+                // swap if next < curr
+                let temp = array[i + 1];
+                array[i + 1] = array[i];
+                array[i] = temp;
+            }
+        }
+        // decrease stopPoint by 1
+        stopPoint -= 1;
+    }
+    return array;
 }
+
+arr1 = [5, 4, 7, 8, 1, 2, 0];
+arr2 = [3, 2, 1];
+
+console.log(bubbleSort(arr2))
 
 module.exports = bubbleSort;
