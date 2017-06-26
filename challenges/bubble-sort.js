fabf132513c1fd33301f573b69a1340049d7f0e3
liyang wang <wangliyang88@gmail.com>
2017-06-26 09:27:53 -0700

    update

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,18 @@
 
 
 function bubbleSort(array) {
-
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < array.length; i++) {
+      if (array[i+1] < array[i]) {
+        let temp = array[i+1];
+        array[i+1] = array[i];
+        array[i] = temp;
+        swapped = true;
+      }
+    }
+  }while(swapped)
 }
 
 module.exports = bubbleSort;
--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -17,15 +17,14 @@ function Node(val) {
 Adds a node to the end of the list
  */
 
-  LinkedList.prototype.add = function(val) {
+LinkedList.prototype.add = function(val) {
   let newNode = new Node(val);
     if (this.head) {
         this.tail.next = newNode;
         newNode.previous = this.tail;
         this.tail = newNode;
     } else {
-        this.head = newNode;
-        this.tail = newNode;
+        this.head = this.tail = newNode;
     }
 };
 
@@ -33,7 +32,28 @@ Adds a node to the end of the list
 Removes the first node with the inputted value
  */
 LinkedList.prototype.remove = function(val) {
-  
+let node = this.head;
+
+  // walk the list
+  while(node) {
+    if(node.val === val) {
+      // if we're not the head, point the one behind us to the one after
+      if(node.prev) node.prev.next = node.next;
+      // if we're not the tail, point the one ahead of us to the one before
+      if(node.next) node.next.prev = node.prev;
+
+      // reset the head/tail as appropriate
+      if(node === this.head) {
+        this.head = node.next;
+      } else if(node === this.tail) {
+        this.tail = node.prev;
+      }
+      // we're done
+      return;
+    }
+    // if we haven't found it, keep looking
+    node = node.next;
+  }
 };
 
 module.exports = LinkedList;
