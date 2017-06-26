4075c69202d7ee3c752647737c3cfd4af66c3def
Poyu Chen <p820316@gmail.com>
2017-06-26 09:11:47 -0700

    Finished bubble-sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,20 @@
 
 
 function bubbleSort(array) {
+  let block = array.length - 1;
+  
+  while (block) {
+    for (let i = 0; i < block; i++) {
+      if (array[i] > array[i + 1]) {
+        let temp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
+      }
+    }
+    block--;
+  }
 
+  return array;
 }
 
 module.exports = bubbleSort;
