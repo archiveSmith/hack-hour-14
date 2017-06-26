4fc4d327cf776e47aadae280cfb3817256769e3f
DebraD <debdo@rocketmail.com>
2017-06-26 09:14:17 -0700

    bubble-sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,19 @@
 
 
 function bubbleSort(array) {
-
+  do {
+    swap = false;
+    for (let i = 0; i < array.length - 1; i++) {
+      if (array[i] > array[i+1]) {
+        let temp = array[i];
+        array[i] = array[i+1];
+        array[i+1] = temp;
+        swap = true;
+      }
+    }
+  } while (swap)
+  
+  return array;
 }
 
 module.exports = bubbleSort;
