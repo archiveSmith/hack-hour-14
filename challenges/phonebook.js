8400480d278729c351e756934669528eb29d7318
John Ivan <johnbauzon@gmail.com>
2017-06-27 09:59:39 -0700

    phonebook

--- challenges/phonebook.js
+++ challenges/phonebook.js
@@ -18,25 +18,77 @@
 *
 * How efficient can you make this?
 
-* Part 2: 
+* Part 2:
 * Why are we storing names and phone numbers in an array?
-* develop a function that takes in the poorly constructed jazbook and returns a proper phonebook 
+* develop a function that takes in the poorly constructed jazbook and returns a proper phonebook
 * complete with methods to add new names and look up and remove existing entries
 */
 
 //  return the number associated with the name in the jazbook
 function findName(jazbook, name) {
-
+  for (let i = 0; i < jazbook.length; i += 1) {
+    if (jazbook[i][0] === name)
+      return jazbook[i][1];
+    return false;
+  }
 }
+
+jazbook = [
+  [
+    'alex', '301-844-3421'
+  ],
+  [
+    'jae', '301-844-1211'
+  ],
+  [
+    'david', '301-844-0978'
+  ],
+  ['travis', '301-844-8505'], ['jasmine', '1800-974-4539']
+];
+
+// console.log(findName(jazbook, 'john'))
 
 // return an object literal representing the jazbook
-function makePhoneBookObject(jazbook){
+function makePhoneBookObject(jazbook) {
+  const jaz = {}
+  for (let i = 0; i < jazbook.length; i += 1) {
+    let name = jazbook[i][0]
+    let num = jazbook[i][1]
+    jaz[name] = num
+  }
+    jaz.add = function(name, phone) {
+      if (!this[name]) {
+        this[name] = phone
+      } else {
+        return "entry already exists"
+      }
+    }
 
+    jaz.find = function(name) {
+      if (this[name])
+        return this[name];
+      return "entry not found"
+    },
+
+    jaz.remove = function(name) {
+      if (this[name]) {
+        delete this[name]
+      } else {
+        return "entry not found"
+      }
+    }
+  return jaz;
 }
+
+// let jaz = makePhoneBookObject(jazbook);
+// jaz.add("john", "1")
+// jaz.remove("john")
+//
+// console.log(jaz)
 
 const objectToExport = {
   findName,
-  makePhoneBookObject,
+  makePhoneBookObject
 };
 
 module.exports = objectToExport;
