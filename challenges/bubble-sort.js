460155053efc3d093c2d7647be0257d33a54231a
Joshua Wolters <joshuawolters@Joshuas-MacBook-Pro.local>
2017-06-27 09:23:27 -0700

    completed bubble-sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,21 @@
 
 
 function bubbleSort(array) {
-
+    let repeat = true;
+    while(repeat){
+        repeat = false;
+        for (let i = 0; i< array.length; i++){
+            if(array[i]>array[i+1]){
+                let holder = array[i];
+                array[i] = array[i+1];
+                array[i+1] = holder;
+                repeat = true
+            }
+        }
+    }
+    return array
 }
+
+console.log(bubbleSort([4,7,12,8,2]))
 
 module.exports = bubbleSort;
