9100333006e9da5bcc50b0dbf05962e6934568cf
Alyssa Gaudioso <alyssa.gaudioso@gmail.com>
2017-06-29 10:06:19 -0700

    new-intersecterions

--- challenges/new-intersections.js
+++ challenges/new-intersections.js
@@ -18,7 +18,35 @@
  */
 
 function newIntersections(x, y){
+    //check for two x coordinates that are the same.
+    //check for two y coordinates that are the same.
+    //^^find all the pairs. check for opposite pairs that are in its range
+    let xPairs = {};
+    let yPairs = {};
+    for(let i=0; i<x.length-1; i++){
+        for(let j=i+1; j<x.length; j++){
+            if(x[i] === x[j]){
+                xPairs[x[i]] = [y[i],y[j]].sort((a, b) => a-b);
+            }
+        }
+    }
+    for(let i=0; i<y.length-1; i++){
+        for(let j=i+1; j<y.length; j++){
+            if(y[i] === y[j]){
+                yPairs[y[i]] = [x[i],x[j]].sort((a,b) => a-b);
+            }
+        }
+    }
 
+    let count = 0;
+    for(let x in xPairs){
+        for(let y in yPairs){
+            if( x > yPairs[y][0] && x < yPairs[y][1] &&
+                y > xPairs[x][0] && y < xPairs[x][1]){
+                count++;
+            }
+        }
+    }
+    return count;
 }
-
 module.exports = newIntersections;
