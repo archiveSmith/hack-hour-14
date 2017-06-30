aa12fa55a7aa3f504ee4b9fff2bb81bec1964743
Poyu Chen <p820316@gmail.com>
2017-06-30 09:14:19 -0700

    merge-ranges

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -1,5 +1,6 @@
 /**
- * Write a function mergeRanges that takes an array of meeting time ranges and returns an array of condensed ranges.
+ * Write a function mergeRanges that takes an array of meeting time ranges
+ * and returns an array of condensed ranges.
  *
  * Example:
  * var times = [[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]]
@@ -10,8 +11,18 @@
  */
 
 
-function mergeRanges(array) {
+const mergeRanges = (array) => {
 
+}
+
+const isInRange = (range, number) => number > range[0] && number < range[1]
+
+const mergeTwoRanges = (range1, range2) => {
+  if (isInRange(range1, range2[0])) {
+    return [[range1[0], range2[1]]]
+  } else {
+    return [range1, range2]
+  }
 }
 
 module.exports = mergeRanges;
