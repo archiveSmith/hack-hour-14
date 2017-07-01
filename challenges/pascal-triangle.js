41c96374447ab383407fd5b10ccc50a9b47348ed
Eva <marinievaluna@gmail.com>
2017-07-01 09:47:47 -0700

    pascal trig

--- challenges/pascal-triangle.js
+++ challenges/pascal-triangle.js
@@ -32,8 +32,44 @@
   ]
 */
 
-function pascalTriangle(numRows) {
+/*  pascalTriangle(6) :
+  [
+    [1],
+    [1,1],
+    [1,2,1],
+    [1,3,3,1],
+    [1,4,6,4,1],
+    [1,5,10,10,5,1]
+  ]
+*/
 
+function pascalTriangle(numRows) {
+  if(numRows === 0) return []
+  if(numRows === 1) return [[1]]
+  
+  let pascal = [];
+
+  for(let i=0; i<numRows; i++) {
+    let row = [];
+    
+    for(let j=0; j<numRows; j++) {
+      console.log("i", i, "j", j)
+      
+      //if first row, 1
+      if(j===0){
+        row.push(1)
+        continue
+      }
+      let newele = row[j-1]* ((i +1 -j)/j)
+      console.log('j is', j, 'new element', newele)
+      if(newele) {
+        row.push(newele)
+      }
+    }
+    pascal.push(row)
+  }
+  return pascal;
 }
+
 
 module.exports = pascalTriangle;
