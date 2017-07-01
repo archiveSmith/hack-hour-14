257e4608f143e9606ad66c6c5acf082cdefbe8dc
Aaron Goldsmith <aarongoldsmith@msn.com>
2017-07-01 09:21:55 -0700

    pascal triangle

--- challenges/pascal-triangle.js
+++ challenges/pascal-triangle.js
@@ -31,8 +31,18 @@
     [1,5,10,10,5,1]
   ]
 */
-
 function pascalTriangle(numRows) {
+  const result = [];
+  result[0] = [1];
+  result[1] = [1, 1];
+  for (let row = 2; row < numRows; row++) {
+    result[row] = [1];
+    for (let col = 1; col <= row - 1; col++) {
+      result[row][col] = result[row - 1][col] + result[row - 1][col - 1];
+      result[row].push(1);
+    }
+  }
+  return result;
 
 }
 
