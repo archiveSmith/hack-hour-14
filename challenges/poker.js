77b7d8b6f28b9bd50a06e802e474cbbe9ad9391e
Eva <marinievaluna@gmail.com>
2017-07-03 11:14:01 -0700

    poker

--- challenges/poker.js
+++ challenges/poker.js
@@ -18,8 +18,118 @@
 * Example: poker([3,5,5,5,2], [4,6,7,8,8]) -> "Player 1 wins"
 */
 
+function Player(hand) {
+  let obj = {}
+  obj.pairNum = 0
+  obj.win ={}
+  obj.highest = ""
+  obj.highestPair = ""
+  obj.cards = hand.reduce((acc, card) => {
+    if(acc.hasOwnProperty(card)) {
+      acc[card] +=1
+      return acc
+    }
+    else {
+      acc[card] = 1
+      return acc
+    }
+  }, {})
+  return obj;
+}
+
+
+function checkStraight(player) {
+  let straight = '234567891011121314'
+  let cards = []
+  for (c in player.cards) {
+    cards.push(c)
+  }
+  cards.join("")
+  if(straight.indexOf(cards) < 0) {
+    return false
+  }
+  else{
+    return Math.max(...cards)
+  }
+}
+
+
+function checkDoubles(player) {
+  let cards = player.cards
+
+    for(card in cards) {
+      
+      //4 of a kind
+      if(cards[card] == 4) {
+        player.win = {type:6, highest: card}      //4 of a kind            
+      }
+      
+      //full house
+      else if(cards[card] == 3 && Object.keys(cards).length > 2) {
+          player.win = {type:5, highest: card}  //full house
+      }
+      
+      //straight
+      else if(checkStraight(player)) {
+        player.win = {type:5, highest: checkStraight(player)}
+      }
+      
+      //3 of a kind
+      else if(cards[card] == 3 && Object.keys(cards).length == 2) {
+        player.win = {type:3, highest: card}                //'3-of-a-kind'
+      }
+      
+      
+      //2 OR 1 pair + find largest card.
+      else if(cards[card] == 2) {
+        
+          player.pairNum += 1
+          player.highestPair = Math.max(player.highestPair, card)
+          
+          if(player.pairNum == 2) {
+            player.win = {type:2, highest:player.highestPair}
+          }
+          else {
+            player.win = {type:1, highest:player.highestPair}
+          }
+      }
+      
+      //HIGHEST CARD 
+     else if(cards[card] == 1) {
+       player.highest = player.highest || card
+       player.win = {type: 0, highest: player.highest}
+       player.highcard = card
+    }
+  }
+}
+
+
 function poker(hand1, hand2) {
 
+  let player1 = new Player(hand1)
+  let player2 = new Player(hand2)
+  
+  checkDoubles(player1)
+  checkDoubles(player2)
+  
+  let winner;
+  if(player1.win.type > player2.win.type) {
+    winner = 'Player 1 wins'
+  }
+  else if (player2.win.type > player1.win.type){
+    winner = 'Player 2 wins'
+  }
+  else {
+      if(player1.win.highest > player2.win.highest) {
+      winner = 'Player 1 wins'
+    }
+    else if (player2.win.highest > player1.win.highest){
+      winner = 'Player 2 wins'
+    }
+  }
+  return winner;
 }
 
 module.exports = poker;
+
+// poker([3,5,5,5,2], [4,6,7,8,8]) // "Player 1 wins"
