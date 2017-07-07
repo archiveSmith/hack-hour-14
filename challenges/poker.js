72811f4f4cc72d69071f71bbed84ca1152ec32d6
liyang wang <wangliyang88@gmail.com>
2017-07-07 11:19:00 -0700

    update

--- challenges/poker.js
+++ challenges/poker.js
@@ -124,3 +124,45 @@ function onePair(hand) {
 }
 
 module.exports = poker;
+
+
+
+
+
+
+
+// //CODESMITH WAY
+// function getScore(hand) {
+//   // Generate an object to represent card frequency
+//   const cardCount = hand.reduce((a, c) => {
+//     a.hasOwnProperty(c) ? a[c] += 1 : a[c] = 1;
+//     return a;
+//   }, {});
+
+//   // Accumulate scores of card count frequency
+//   let score = 0;
+
+//   for (const card in cardCount) {
+//     switch (cardCount[card]) {
+//       case 4: score += 500; break;
+//       case 3: score += 300; break;
+//       case 2: score += 100; break;
+//       default: break;
+//     }
+//   }
+
+//   // straight
+//   hand.sort((a, b) => b - a);
+//   if (score === 0 && hand[0] - hand[4] === 4) score += 350;
+
+//   // add high card
+//   return score + hand[0];
+// }
+
+// function poker(hand1, hand2) {
+//   const player1 = getScore(hand1);
+//   const player2 = getScore(hand2);
+
+//   if (player1 === player2) return 'Draw';
+//   return player1 > player2 ? 'Player 1 wins' : 'Player 2 wins';
+// }
