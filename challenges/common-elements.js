493b2441291c30714e722c76883a1103a295a51a
DebraD <debdo@rocketmail.com>
2017-07-10 09:35:45 -0700

    common-elements

--- challenges/common-elements.js
+++ challenges/common-elements.js
@@ -13,7 +13,41 @@
 // if there are no common numbers or strings return the string "Nothing in Common!"
 
 function commonElements(array1, array2, array3, array4){
-
+  let itemCount = {};
+  
+  let arrayList = [array1, array2, array2, array4];
+  
+  arrayList.forEach(function(array) {
+    let temp = {};
+    for (let i = 0; i < array.length; i += 1) {
+      if (array[i] in temp) {
+        temp[array[i]] += 1;
+      }
+      else {
+        temp[array[i]] = 1;
+      }
+    }
+    let keys = Object.keys(temp);
+    for (let j = 0; j < keys.length; j += 1) {
+      if (keys[j] in itemCount) {
+        itemCount[keys[j]] += 1;
+      }
+      else {
+        itemCount[keys[j]] = 1;
+      }
+    }
+  });
+  
+  let items = Object.keys(itemCount);
+  let returnArray = [];
+  
+  for (let i = 0; i < items.length; i += 1) {
+    if (itemCount[items[i]] === 4) {
+      returnArray.push(items[i]);
+    }
+  }
+  
+  return returnArray.length > 0 ? returnArray : "Noting in Common!";
 }
 
 module.exports = commonElements;
