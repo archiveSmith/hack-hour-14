6d97f428eb70b4b90b73bd159c874d896486785c
Aaron Goldsmith <aarongoldsmith@msn.com>
2017-07-13 09:16:55 -0700

    each perm

--- challenges/each-permutation.js
+++ challenges/each-permutation.js
@@ -22,6 +22,29 @@ eachPermutation([1, 2, 3], function(perm) {
 
 function eachPermutation(arr, callback) {
 
+  let myPerms = permutator(arr)
+  myPerms.forEach(perm => {
+    console.log(perm)
+  })
+
+}
+
+
+const permutator = (inputArr) => {
+  let result = [];
+  const permute = (arr, m = []) => {
+    if (arr.length === 0) {
+      result.push(m)
+    } else {
+      for (let i = 0; i < arr.length; i++) {
+        let curr = arr.slice();
+        let next = curr.splice(i, 1);
+        permute(curr.slice(), m.concat(next))
+      }
+    }
+  }
+  permute(inputArr)
+  return result;
 }
 
 
--- challenges/knapsack-problem.js
+++ challenges/knapsack-problem.js
@@ -9,11 +9,25 @@
   solveKnapsack(items, 5); // returns 9 (from items[1] and items[2])
 */
 
-function solveKnapsack(items, weightAvailable) {
 
 
 
+function solveKnapsack(items, weightLeft) {
+  //base case: if there are no items or no weight
+  if (items.length === 0 || weightLeft <= 0) return 0
+
+  //if firts item is too heavey to fit, disregard it and consider other itmes
+  if (items[0].weight > weightLeft) {
+    return solveKnapsack(items.slice(1), weightLeft)
+  }
+
+  //if first item does fit
+  const itemsLeft = items.slice(1);
+  const includeItem = items[0].value + solveKnapsack(itemsLeft, weightLeft - items[0].weight)
+  const leaveItem = solveKnapsack(itemsLeft, weightLeft)
+  //return the higher value of leaving vs including the item
+  return includeItem > leaveItem ? includeItem : leaveItem
 }
-;
+
 
 module.exports = solveKnapsack;
