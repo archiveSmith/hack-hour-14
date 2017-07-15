96bbf565c3c26a436d2bff7bee2e132b5f1fed84
Pineapple <Pineapple@Users-MacBook-Pro.local>
2017-07-15 09:10:01 -0700

    string-rotation refactor

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -14,13 +14,34 @@
 function isSubstring(s1, s2) {
   return s1.indexOf(s2) >= 0;
 }
+//if s1 === s2 return true
+//combine s1
+//insert str and s2 into isSubstring to check if there is an index
+//if is substring is greater than -1 return true else false
+
 
 function stringRotation(s1, s2) {
-    if(s1.length !== s2.length) return false; 
-    
-    let superS1 = s1+ s1; 
-  
-    return isSubstring(superS1,s2); 
+ if(s1 === s2) return true; 
+ if(s1.length !== s2.length) return false; 
+
+ return isSubstring((s1+s1), s2);
+
 }
 
+console.log(stringRotation("hello", "hello")) //-> true
+              console.log(stringRotation("hello", "llohe")) //-> true
+
+              console.log(stringRotation("hello", "he")) //-> false
+              console.log(stringRotation("hello", "ollhe")) //-> false (not a rotation, just an anagram)
+
 module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
+
+
+
+// function stringRotation(s1, s2) {
+//     if(s1.length !== s2.length) return false; 
+    
+//     let superS1 = s1+ s1; 
+  
+//     return isSubstring(superS1,s2); 
+// }
