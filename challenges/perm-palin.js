67769205fce090bf37824d50a779e2faf5d6626f
jacobworrel <jacobworrel@gmail.com>
2017-07-28 16:42:00 -0700

    refactor perm-palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -9,10 +9,22 @@
  *
  */
 
+ //if more than one letter only appears once, return false
  function permPalin(str) {
-   //loop through string
-   //check if letter shows up again
-   //if not return false
+   const cache = {};
+   let counter = 0;
+   for (let i = 0; i < str.length; i += 1) {
+     if (cache[str[i]]) cache[str[i]] += 1;
+     else cache[str[i]] = 1;
+   }
+   for (letter in cache) {
+     if (cache[letter] === 1) counter += 1;
+   }
+   if (counter > 1) return false;
+   else return true;
  }
 
- console.log(permPalin('racecar'));
+ console.log(permPalin('abab')) // => true
+ console.log(permPalin('cbaba')) // => true
+ console.log(permPalin('cbac')) // => false
+ console.log(permPalin('a')) // => true
