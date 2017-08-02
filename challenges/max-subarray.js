521a3c82fc4876a6c92a02edbbab28131a16be5a
jacobworrel <jacobworrel@gmail.com>
2017-08-02 10:08:02 -0700

    ready for approach

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -7,8 +7,52 @@
  *
  */
 
-function maxSubarray(arr) {
+//using 2 loops (brute force)
+//calculate sum of every possible subarray
+//if currMax > finalMax, assign currMax to finalMax
+//
+const maxSubarray = (arr) => {
+  //declare finalMax variable and initialize to -Infinity
+  //declare currMax variable
+  let finalMax = -Infinity;
+  let currMax;
 
+  //loop through array
+  for (let i = 0; i < arr.length; i += 1) {
+    //set currMax = 0
+    currMax = 0;
+    //slice array from i to the end
+    //for each item in subarray, add item to currMax
+    //if currMax > finalMax, set finalMax = currMax
+    arr.slice(i).forEach((item) => {
+      currMax += item;
+      if (currMax > finalMax) finalMax = currMax;
+    })
+  }
+  //return finalMax
+  return finalMax;
 }
+
+ //using 1 loop
+
+ // const maxSubarray = (arr) => {
+ //   //declare currMax and finalMax variables and initialize to -Infinity
+ //   let finalMax = -Infinity;
+ //   let currMax = -Infinity;
+ //   //loop through arr using forEach()
+ //   arr.forEach((item) => {
+ //     //each iteration, compare current element vs. currMax + current element
+ //     //set currMax to the larger num of the two
+ //     currMax = Math.max(item, currMax + item);
+ //     //compare finalMax vs. currMax
+ //     //set finalMax to the larger num of the two
+ //     finalMax = Math.max(finalMax, currMax);
+ //   });
+ //   //return finalMax
+ //   return finalMax;
+ // }
+
+ console.log(maxSubarray([1, -2, 3, 10, -4, 7, 2, -5])); //-> 18 from [3, 10, -4, 7, 2]
+ console.log(maxSubarray([15,20,-5,10]));  //-> 40
 
 module.exports = maxSubarray;
