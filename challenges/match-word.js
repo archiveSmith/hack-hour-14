e5e72768f1a06e7104e57d2832631dc61d13c384
jacobworrel <jacobworrel@gmail.com>
2017-08-11 11:55:41 -0700

    ready for approach

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -1,12 +1,49 @@
-// Some languages have "if" statements that are closed by "fi" instead of curly brackets. Or they close a "case" with "esac", i.e. the same keyword backwards. for this problem we'll check that all words in a string are "closed". Write a function that takes a string and returns true if every word is closed by its backwards counterpart. Words must be separated by space or punctuation.
+// Some languages have "if" statements that are closed by "fi" instead of curly brackets.
+//Or they close a "case" with "esac", i.e. the same keyword backwards.
+//for this problem we'll check that all words in a string are "closed".
+//Write a function that takes a string and returns true if every word is closed by its backwards counterpart.
+//Words must be separated by space or punctuation.
 
 // matchWord('__END_DNE-----');  -> true
 // matchWord('__ENDDNE__');  -> false       (not separated by a space)
 // matchWord('IF()()fi[]');  -> true        (should be case-insensitive)
-<<<<<<< HEAD
 // matchWord('for__if__rof__fi');  -> false     not properly closed. like ( [) ]
 // matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw');  -> true
 // matchWord('');  -> true
+
+const matchWord = (str) => {
+  //edge case: empty string
+  if (str === '') return true;
+  //convert str to lower case, split it at non [a-z] characters, and then filter out empty strings
+  let wordArr = str.toLowerCase().split(/[^a-z]/).filter((item) => item !== '');
+  //if wordArr.length is uneven number, return false
+  if (wordArr.length % 2 === 1) return false;
+  //initialize stack as empty array
+  const stack = [];
+  //loop through wordArr
+  for (let i = 0; i < wordArr.length; i += 1) {
+    //if last item in stack === reverse of current item in wordArr, pop it off the stack
+    if (stack[stack.length - 1] === wordArr[i].split('').reverse().join('')) stack.pop();
+    //else, push current item in wordArr onto stack
+    else stack.push(wordArr[i]);
+  }
+  //if stack is empty, return true, else, return false
+  return !stack.length;
+}
+
+console.log(matchWord('__END_DNE-----'));  //-> true
+console.log(matchWord('__ENDDNE__'));  //-> false       (not separated by a space)
+console.log(matchWord('IF()()fi[]'));  //-> true        (should be case-insensitive)
+console.log(matchWord('for__if__rof__fi'));  //-> false     not properly closed. like ( [) ]
+console.log(matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw'));  //-> true
+
+
+
+
+
+
+
+
 
 // MY SOLUTION:
 
@@ -29,32 +66,24 @@
 
 // FELLOW SOLUTION:
 
-function matchWord(str) {
-  if (!str) return true;
-  const strArr = str.replace(/[^\w\s]|/g, '').replace(/\s+|_/g, ' ').toLowerCase().split(' ');
-  if (strArr.length % 2 === 1) return false;
-  const checkArr = [strArr[0]];
-  let j;
-  for (j = 1; j < str.length; j += 1) {
-    let reversed = strArr[j].split('').reverse().join('');
-    if (reversed === checkArr[checkArr.length - 1]) {
-      checkArr.pop()
-    } else {
-      checkArr.push(strArr[j]);
-    }
-  }
-  return checkArr.length === 0;
-}
+// function matchWord(str) {
+//   if (!str) return true;
+//   const strArr = str.replace(/[^\w\s]|/g, '').replace(/\s+|_/g, ' ').toLowerCase().split(' ');
+//   console.log(strArr)
+//   if (strArr.length % 2 === 1) return false;
+//   const checkArr = [strArr[0]];
+//   let j;
+//   for (j = 1; j < str.length; j += 1) {
+//     let reversed = strArr[j].split('').reverse().join('');
+//     if (reversed === checkArr[checkArr.length - 1]) {
+//       checkArr.pop()
+//     } else {
+//       checkArr.push(strArr[j]);
+//     }
+//   }
+//   return checkArr.length === 0;
+// }
 
-console.log(matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw'));
-=======
-// matchWord('for__if__rof__fi');  -> false     not properly closed. like ( [) ] 
-// matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw');  -> true
-// matchWord('');  -> true
-
-function matchWord(str) {
-
-}
->>>>>>> c4f344d6fc53288e9ce1210315367f0c6dea1718
+// console.log(matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw'));
 
 module.exports = matchWord;
