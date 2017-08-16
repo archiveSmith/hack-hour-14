3b94915fbe00d8cf7709cc0248c413ed61b0dc7d
Liyang Wang <lng@Liyangs-MacBook-Pro-2017.local>
2017-08-16 12:25:43 -0700

    update

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,26 @@
 
 
 function modemean(array) {
+  let mean = Math.floor(array.reduce((a,c) => {
+    return a+c
+  })/array.length)
+  
+  let mode = array.reduce((a,c) => {
+    if (!a[c]) {
+      a[c] = 0
+    }
+    a[c]+=1
+    return a;
+  }, {})
+  
+  let count = 0;
+  for (x in mode) {
+    if(mode[x] > count) {
+      count = x
+    }
+  }
 
+  return mean === parseInt(count) ? true : false;
 }
 
 module.exports = modemean;
