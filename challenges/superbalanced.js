77569681662d000fdf9433a0f07d6d412a4a3b5d
jacobworrel <jacobworrel@gmail.com>
2017-10-01 11:45:39 -0700

    fail fast optimization

--- challenges/superbalanced.js
+++ challenges/superbalanced.js
@@ -7,27 +7,54 @@
 example: http://www.geeksforgeeks.org/wp-content/uploads/balanced_tree.GIF
  */
 
-function BinaryTree(value) {
-  this.value = value;
-  this.left = null;
-  this.right = null;
+class BinaryTree {
+ constructor(value) {
+   this.value = value;
+   this.left = null;
+   this.right = null;
+ }
 }
 
-const getHeight = (tree) => {
-  if (!tree) return -1;
-  let left = getHeight(tree.left);
-  let right = getHeight(tree.right);
-  return Math.max(left, right) + 1;
+const superbalanced = (tree) => {
+  const checkBalance = (tree) => {
+    if (!tree) return 0;
+    const left = checkBalance(tree.left);
+    if (left === 'unbalanced') return false;
+    const right = checkBalance(tree.right);
+    if (right === 'unbalanced') return false;
+    if (left - right > 1 || right - left > 1) return 'unbalanced';
+    else return 1 + Math.max(left, right);
+  }
+  return checkBalance(tree) ? true : false;
 }
 
-function superbalanced(tree) {
-  const leftHeight = getHeight(tree.left);
-  const rightHeight = getHeight(tree.right);
-  if (leftHeight - rightHeight > 1 || rightHeight - leftHeight > 1) return false;
-  else return true;
-}
 
-const tree = {value: 1, left: {value: 2, left: {value: 3, left: null, right: null}, right: null}, right: {value: 5, left: null, right: null}};
-console.log(superbalanced(tree));
+
+// function BinaryTree(value) {
+//   this.value = value;
+//   this.left = null;
+//   this.right = null;
+// }
+
+// const getHeight = (tree) => {
+//   if (!tree) return -1;
+//   let left = getHeight(tree.left);
+//   let right = getHeight(tree.right);
+//   return Math.max(left, right) + 1;
+// }
+
+// function superbalanced(tree) {
+//   const leftHeight = getHeight(tree.left);
+//   const rightHeight = getHeight(tree.right);
+//   if (leftHeight - rightHeight > 1 || rightHeight - leftHeight > 1) return false;
+//   else return true;
+// }
+
+const balancedTree = {value: 5, left: {value: 3, left: {value: 2, left: null, right: null}, right: null}, right: {value: 7, left: null, right: null}};
+const unbalancedTree = {value: 5, left: {value: 3, left: {value: 2, left: { value: 1, left: null, right: null}, right: null}, right: null}, right: {value: 7, left: null, right: null}};
+// console.log(JSON.stringify(balancedTree, null, 2));
+console.log(JSON.stringify(unbalancedTree, null, 2));
+console.log(superbalanced(balancedTree));
+console.log(superbalanced(unbalancedTree));
 
 module.exports = {BinaryTree: BinaryTree, superbalanced: superbalanced};
